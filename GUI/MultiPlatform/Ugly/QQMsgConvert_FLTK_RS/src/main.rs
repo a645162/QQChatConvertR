5e9a60e57e41943de85acd2a7525cdefeec8c503
Haomin Kong <a645162@qq.com>
2023-10-02 23:32:40 +0800

    搞定大体的UI框架！

--- GUI/MultiPlatform/Ugly/QQMsgConvert_FLTK_RS/src/main.rs
+++ GUI/MultiPlatform/Ugly/QQMsgConvert_FLTK_RS/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+use std::process::exit;
 // src/main.rs
 use fltk::{prelude::*, *};
 
@@ -7,8 +9,33 @@ mod ui1;
 fn main() {
     let app = app::App::default();
     let mut ui1 = ui1::UserInterface::make_window();
-    //
-    //
+
+
+    ui1.menubar.find_item("文件/打开MHT文件").unwrap().set_callback(move |_| {
+        let mut dialog =
+            dialog::NativeFileChooser::new(
+                dialog::NativeFileChooserType::BrowseFile
+            );
+        // dialog.set_directory("");
+        dialog.set_filter("MHT Files\t*.mht");
+        dialog.show();
+
+        println!("选择MHT文件: {}", dialog.filename().to_str().unwrap());
+        ui1.input_mht_path.set_value(dialog.filename().to_str().unwrap());
+    });
+
+    ui1.menubar.find_item("文件/关闭程序").unwrap().set_callback(move |_| {
+        program_end();
+    });
+
+    ui1.menubar.find_item("帮助/关于").unwrap().set_callback(move |_| {
+        ui1::UserInterface_About::make_window_about().window_about.show();
+    });
+
     ui1.main_window.show();
     app.run().unwrap();
+}
+
+fn program_end() {
+    exit(0);
 }
